6a86589a7d7a580b72b001c41ca4217ccedf4e4b
glauco.developer@gmail.com <glauco.developer@gmail.com>
2020-01-31 23:57:22 +0000

    RestAPI GET method

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'glauco' },
+    { id: 2, name: 'russo' },
+];
+
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+})
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
